b25e596a927b34419a549d49f65e6bc0d95c7f4d
kapolious <karolina.pirohova@gmail.com>
2026-01-19 11:11:25 +0100

    exclude unfinished

--- app/getArticles.tsx
+++ app/getArticles.tsx
@@ -2,6 +2,8 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 
+const EXCLUDED_FILES = ['88.md', '100.md'];
+
 interface Article {
   slug: string;
   title: string;
@@ -16,7 +18,9 @@ export function getArticles(): Article[] {
   const dir = path.join(process.cwd(), "app/reflexie/content");
   const files = fs.readdirSync(dir);
 
-  return files.map(filename => {
+  return files
+  .filter(filename => !EXCLUDED_FILES.includes(filename))
+  .map(filename => {
     const file = fs.readFileSync(path.join(dir, filename), "utf-8");
     const { data, content } = matter(file);
 
